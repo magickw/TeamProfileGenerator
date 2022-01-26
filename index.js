cdb0c6ada1c8cd7592dd7c9c63554ab3ddae6cd6
Baofeng Guo <peterguo1983@gmail.com>
2022-01-26 15:03:32 -0800

    Added the engineer section.

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const teamMembers = [];
 const idArray = [];
 
 
-//Run create manager functions
+//Run createManager functions
 function createManager() {
-    //Prompt to answer the question about the manager
+    //Prompt to answer the questions about the manager
     inquirer.prompt([
       {
         type: "input",
@@ -40,12 +40,103 @@ function createManager() {
       {
         type: "input",
         name: "managerOfficeNumber",
-        message: "What is the team manager's office number?",
+        message: "What is the manager's office number?",
       }
     ]).then(answers => {
       const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
       teamMembers.push(manager);
       idArray.push(answers.managerId);
+      //Once the manager role is created, then run the createTeam function
+      createTeam();
+    });
+  }
+
+  //Run createTeam() function
+  function createTeam() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberChoice",
+        //Prompt to add the type of team member
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ]).then(userChoice => {
+      switch (userChoice.memberChoice) {
+          //Run the corresponding function according to the user's choice
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+  }
+
+//Run addEngineer() function when the user chooses Engineer
+  function addEngineer() {
+      //Prompt to answer the questions about the manager
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's ID?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+              //Check if the id has been taken
+            if (idArray.includes(answer)) {
+                //Prompt the user to choose new id if the id has been taken
+              return "This ID is already taken. Please enter a different ID.";
+            } else {
+              return true;
+            }
+
+          }
+          //Prompt the user to enter a positive number
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's GitHub username?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      }
+    ]).then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+      teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
       createTeam();
     });
   }
