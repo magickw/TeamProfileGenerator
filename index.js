af839716b2c89d0efaf73e0f2709669b28e33717
Baofeng Guo <peterguo1983@gmail.com>
2022-01-25 21:49:14 -0800

    Updated the index.js.

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+//Import the employees
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+//Import the inquirer module
 const inquirer = require("inquirer");
-const path = require("path");
+//Import the file system module
 const fs = require("fs");
+
+//Create a team member array
+const teamMembers = [];
+const idArray = [];
+
+
+//Run create manager functions
+function createManager() {
+    //Prompt to answer the question about the manager
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's ID?",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?",
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?",
+      }
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamMembers.push(manager);
+      idArray.push(answers.managerId);
+      createTeam();
+    });
+  }
+
+  
